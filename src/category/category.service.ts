79da9046fa9dbde36baa5e5057673a043d4abbf3
Kelthon <kelthonbalbino@gmail.com>
2024-04-22 22:14:15 -0300

    feat: create, findAll and findOne category service methods was updated

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CategoryService {
+  constructor(
+    @InjectRepository(Category) private repository: Repository<Category>,
+  ) {}
+
   create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+    const category = this.repository.create(createCategoryDto);
+    return this.repository.save(category);
   }
 
   findAll() {
-    return `This action returns all category`;
+    return this.repository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    return this.repository.findOneBy({ id });
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
